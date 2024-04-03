2472e284e74e7235e354f82440a6c3b321cac43a
AntLrc <antoine.leclerc@unil.ch>
2024-04-03 23:09:08 +0200

    Adding a file

---
+++ Storm_tracks/extracting_v1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import h5py
+
+# path = "/work/FAC/FGSE/IDYST/tbeucler/downscaling/raw_data/severe_storms_2016-2021/CH_severe_storms_2016_2021.csv"
+path = ""
+
+tracks = pd.read_csv(path)
+
+
+
+print("test")
+
+

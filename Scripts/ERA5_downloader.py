62d6039f3bd33a627066340a38c24f8223c229a6
AntLrc <antoine.leclerc@unil.ch>
2024-04-17 16:49:35 +0200

    finishing pipe

--- Scripts/ERA5_downloader.py
+++ Scripts/ERA5_downloader.py
@@ -47,6 +47,10 @@ parser.add_argument("--time",
                     default=False,
                     help = "Hour to download, format HH")
 
+parser.add_argument("--test",
+                    action="store_true",
+                    help="Test the downloader.")
+
 args = parser.parse_args()
 
 folder_path =  args.output
@@ -54,38 +58,45 @@ folder_path =  args.output
 # Choose what dates to download depending on parsed arguments.
 if args.from_file:
     dates1 = pd.read_csv(args.path, header = None, index_col=0, parse_dates=True).index
-
+else:
+    dates1 = pd.Series()
 
 if args.date:
     year = args.date[:4]
     if len(args.date) == 4:
         if args.time:
-            dates = pd.date_range(start = year + "-01-01 " + args.time + ":00", end = year + "-12-31 " + args.time + ":00", freq = "h")
+            dates = pd.date_range(start = year + "-01-01 " + args.time + ":00", end = year + "-12-31 " + args.time + ":00", freq = "h", tz='utc')
         else:
-            dates = pd.date_range(start = year + "-01-01 00:00", end = year + "-12-31 23:00", freq = "h")
+            dates = pd.date_range(start = year + "-01-01 00:00", end = year + "-12-31 23:00", freq = "h", tz='utc')
             
     elif len(args.date) == 6:
         month = args.date[4:]
         if args.time:
-            dates = pd.date_range(start = year + "-" + month + "-01 " + args.time + ":00", end = year + "-" + month + "-31 " + args.time + ":00", freq = "h")
+            dates = pd.date_range(start = year + "-" + month + "-01 " + args.time + ":00", end = year + "-" + month + "-31 " + args.time + ":00", freq = "h", tz='utc')
         else:
-            dates = pd.date_range(start = year + "-" + month + "-01 00:00", end = year + "-" + month + "-31 23:00", freq = "h")
+            dates = pd.date_range(start = year + "-" + month + "-01 00:00", end = year + "-" + month + "-31 23:00", freq = "h", tz='utc')
             
     elif len(args.date) == 8:
         month = args.date[4:6]
         day = args.date[6:]
         if args.time:
-            dates = pd.date_range(start = year + "-" + month + "-" + day + " " + args.time + ":00", end = year + "-" + month + "-" + day + " " + args.time + ":00", freq = "h")
+            dates = pd.date_range(start = year + "-" + month + "-" + day + " " + args.time + ":00", end = year + "-" + month + "-" + day + " " + args.time + ":00", freq = "h", tz='utc')
         else:
-            dates = pd.date_range(start = year + "-" + month + "-" + day + " 00:00", end = year + "-" + month + "-" + day + " 23:00", freq = "h")
+            dates = pd.date_range(start = year + "-" + month + "-" + day + " 00:00", end = year + "-" + month + "-" + day + " 23:00", freq = "h", tz='utc')
     
     else:
         raise ValueError("Date format not recognized.")
     
-else:
-    if dates1.
-    dates = pd.date_range(start = (pd.Timestamp.today() - pd.Timedelta(hours = 24)).strftime("%Y-%m-%d %H"), end = pd.Timestamp.today(), freq = "H")
+    if not(dates1.empty):
+        dates = dates.intersection(dates1)
     
+else:
+    if dates1.empty:
+        dates = pd.date_range(start = (pd.Timestamp.today() - pd.Timedelta(hours = 24)).strftime("%Y-%m-%d %H"), end = pd.Timestamp.today(), freq = "H")
+    else:
+        dates = dates1
+
+
 # Load the dates already downloaded.    
 when = pd.read_csv("/work/FAC/FGSE/IDYST/tbeucler/downscaling/raw_data/AI-models-input/when.csv", header = None, index_col=0, parse_dates=True).index
 
@@ -99,43 +110,44 @@ dates_gr = dates.groupby(dates.date)
 
 for date in dates_gr:
     group = dates_gr[date]
-    """
+    
     try:
-    """  
-    year, month, day = date.strftime("%Y"), date.strftime("%m"), date.strftime("%d")
-    
-    print("\nDownloading " + date.strftime("%Y-%m-%d") + "...")  
-    date = date.strftime("%Y-%m-%d")
-    hours = list(group.strftime("%H:%M"))
-    
-    
-    for data_origin, variable, variables in datas:
-        subprocess.run(["mkdir", "-p", os.path.join(folder_path, variable, year, month)])
+        year, month, day = date.strftime("%Y"), date.strftime("%m"), date.strftime("%d")
         
-        variables_to_download = {
-            'product_type': 'reanalysis',
-            'format': 'grib',
-            'variable': variables,
-            'year': year,
-            'month': month,
-            'day': day,
-            'time': hours,
-        }
-        variables_to_download.update(({'pressure_level': ['1000', '925', '850', '700', '600', '500', '400', '300', '250', '200', '150', '100', '50']} if variable == "pressure_variables" else {}))
+        print("\nDownloading " + date.strftime("%Y-%m-%d") + "...")  
+        date = date.strftime("%Y-%m-%d")
+        hours = list(group.strftime("%H:%M"))
         
-        c.retrieve(
-        data_origin,
-        variables_to_download,
-        target=os.path.join(folder_path, variable, year, month, date + "_ERA5.grib")
-        )
-    
-    when = np.sort(np.unique(np.concatenate([when, group])))
-    """
+        if not(args.test):
+            for data_origin, variable, variables in datas:
+                subprocess.run(["mkdir", "-p", os.path.join(folder_path, variable, year, month)])
+                
+                variables_to_download = {
+                    'product_type': 'reanalysis',
+                    'format': 'grib',
+                    'variable': variables,
+                    'year': year,
+                    'month': month,
+                    'day': day,
+                    'time': hours,
+                }
+                variables_to_download.update(({'pressure_level': ['1000', '925', '850', '700', '600', '500', '400', '300', '250', '200', '150', '100', '50']} if variable == "pressure_variables" else {}))
+                
+                c.retrieve(
+                data_origin,
+                variables_to_download,
+                target=os.path.join(folder_path, variable, year, month, date + "_ERA5.grib")
+                )
+            
+            when = np.sort(np.unique(np.concatenate([when, group])))
+        else:
+            print(f"Test: would have downloaded {date}.")
+        
     except:
         print("Error downloading " + str(date) + ".")
         with open("/work/FAC/FGSE/IDYST/tbeucler/downscaling/alecler1/repos/Scripts/Outputs/failed.txt", 'a') as file:
             file.write(str(date) + "\n")
-    """
+    
 
 pd.Series(when).to_csv("/work/FAC/FGSE/IDYST/tbeucler/downscaling/raw_data/AI-models-input/when.csv", index = False, header=False)
 
